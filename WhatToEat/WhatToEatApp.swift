9ae22916c2837674867d3effd507ed8c469ab670
Theordius <87307483+Theordius@users.noreply.github.com>
2022-03-21 14:09:13 +0100

    Initial Commit

---
+++ WhatToEat/WhatToEatApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatToEatApp.swift
+//  WhatToEat
+//
+//  Created by Rafał Gęsior on 21/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct WhatToEatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
