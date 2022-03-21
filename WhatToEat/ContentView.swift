7456afbca31412fe9c8e58407ad2cb81ae0df1ce
Theordius <87307483+Theordius@users.noreply.github.com>
2022-03-21 14:46:33 +0100

    Complete code and rename images to comply

--- WhatToEat/ContentView.swift
+++ WhatToEat/ContentView.swift
@@ -8,14 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var brandLogo = 1
+   
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+            VStack {
+                Image("app_logo")
+                Spacer()
+                HStack {
+                    DiceView(n: brandLogo)
+                    
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button(action: {
+                    self.brandLogo = Int.random(in: 1...6)
+                   
+                }) {
+                    Text("What we gonna eat?")
+                        .font(.system(size: 20))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                }
+                .background(Color.green)
+                
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct DiceView: View {
+    
+    let n: Int
+    
+    var body: some View {
+        Image("logo_\(n)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding()
     }
 }
