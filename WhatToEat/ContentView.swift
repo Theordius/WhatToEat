4ac167667393c6900684e3e257fcbe9e424d6bb3
Theordius <87307483+Theordius@users.noreply.github.com>
2022-03-21 15:17:52 +0100

    Improve UI Design

--- WhatToEat/ContentView.swift
+++ WhatToEat/ContentView.swift
@@ -13,50 +13,62 @@ struct ContentView: View {
    
     var body: some View {
         ZStack {
-            Image("background")
-                .resizable()
-                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+//            Image("background")
+//                .resizable()
+//                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
             
-            VStack {
+            VStack(spacing: 50.0) {
                 Image("app_logo")
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 100, height: 100, alignment: .top)
+                
                 Spacer()
+                
                 HStack {
-                    DiceView(n: brandLogo)
+                    Button(action: {
+                        self.brandLogo = Int.random(in: 1...6)
+                       
+                    }) {
+                        Text("What we gonna eat?")
+                            .font(.system(size: 20))
+                            .fontWeight(.heavy)
+                            .foregroundColor(.white)
+                            .padding(.horizontal)
+                    }
+                    .background(Color.green)
                     
                 }
-                .padding(.horizontal)
-                Spacer()
-                Button(action: {
-                    self.brandLogo = Int.random(in: 1...6)
-                   
-                }) {
-                    Text("What we gonna eat?")
-                        .font(.system(size: 20))
-                        .fontWeight(.heavy)
-                        .foregroundColor(.white)
-                        .padding(.horizontal)
-                }
-                .background(Color.green)
+            
+                BrandView(n: brandLogo)
+                    .padding(.horizontal)
+                
                 
             }
         }
+        .padding(.all)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            
+            
     }
 }
 
-struct DiceView: View {
+struct BrandView: View {
     
     let n: Int
     
     var body: some View {
         Image("logo_\(n)")
+           
+            .renderingMode(.none)
             .resizable()
-            .aspectRatio(1, contentMode: .fit)
+            .aspectRatio(1, contentMode: .fill)
+            .aspectRatio(contentMode: .fit)
             .padding()
+            .frame(width: 125.0, height: 125.0)
     }
 }
